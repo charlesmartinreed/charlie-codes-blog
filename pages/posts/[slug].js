ace220c5676f0560ca698fd8e08452adfe0dc00f
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-07-21 22:05:27 -0500

    Create shareSheet utility module.

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -3,49 +3,17 @@ import Head from "next/head";
 import Script from "next/script";
 import Layout from "../../components/Layout";
 import styles from "../../styles/Post.module.css";
+import { handleSharing } from "../../lib/shareSheets";
 
 import { FaFacebook, FaTwitter, FaShare, FaHome } from "react-icons/fa";
 
 export default function FirstPost() {
-  const title = "First Post";
   return (
     <Layout title={title}>
       <div className={styles.container}>
         <div className={styles.postContainer}>
-          <h1 className={styles.title}>{title}</h1>
-          <p className={styles.postContents}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid
-            voluptatibus, accusamus animi quia ex suscipit dicta fugiat error
-            deleniti commodi adipisci ut eligendi minus alias.
-          </p>
-          <p className={styles.postContents}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
-            a repellat culpa debitis eum assumenda earum sit architecto quia
-            suscipit! Ipsum asperiores sed nobis quisquam beatae fuga illum
-            adipisci, exercitationem corporis recusandae. Animi asperiores nam
-            odio maiores repellendus ratione enim vitae iste, perspiciatis ullam
-            vero quod nulla doloribus iure eius aut repellat quas pariatur
-            voluptate porro magnam voluptas. Sint expedita blanditiis modi
-            tenetur. Sed, commodi blanditiis iure cum fugit perspiciatis saepe
-            corrupti quod fuga non eaque reiciendis dolor possimus quasi, at
-            modi accusamus. Magnam minus, modi assumenda consequatur laboriosam
-            quam debitis error porro, ut consectetur expedita blanditiis sunt
-            veniam consequuntur beatae dicta quis voluptatum nisi illo?
-            Temporibus perspiciatis maiores sequi non quas nemo dolores iure
-            delectus. Ipsa ad ipsam ab sunt placeat? Vitae, reprehenderit ea?
-          </p>
-          <p className={styles.postContents}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Recusandae
-            error culpa voluptatum quo accusamus. Repudiandae, quo doloremque!
-            Quam voluptatem eaque culpa blanditiis, deleniti, incidunt eos nulla
-            eligendi accusantium ab rem facilis quidem aut velit dolor alias
-            quis! Iusto impedit minus ab atque totam laudantium doloremque
-            reprehenderit optio est temporibus excepturi sequi dolore aspernatur
-            omnis numquam ipsa, non perferendis dicta ex! Necessitatibus,
-            laboriosam omnis quia earum, fugit numquam ex inventore cupiditate
-            ab id alias eos dolores harum eum fuga ad maiores ut doloribus.
-            Ullam in reiciendis hic?
-          </p>
+          <h1 className={styles.title}>{"TITLE GOES HERE"}</h1>
+          <p className={styles.postContents}>{" CONTENT GOES HERE "}</p>
         </div>
         <div className={styles.iconContainer}>
           <Link href="/">
@@ -53,17 +21,37 @@ export default function FirstPost() {
               <FaHome className={styles.icons} />
             </a>
           </Link>
-          <a href="#!" title="Share on Facebook">
+          <a
+            href="#!"
+            title="Share on Facebook"
+            onClick={() => handleSharing("facebook")}
+          >
             <FaFacebook className={styles.icons} />
           </a>
-          <a href="#!" title="Share on Twitter">
+          <a
+            href="#!"
+            title="Share on Twitter"
+            onClick={() => handleSharing("twitter")}
+          >
             <FaTwitter className={styles.icons} />
           </a>
-          <a href="#!" title="Copy Link To Blog Post">
+          <a
+            href="#!"
+            title="Copy Link To Blog Post"
+            onClick={() => handleSharing("web")}
+          >
             <FaShare className={styles.icons} />
           </a>
         </div>
       </div>
     </Layout>
   );
+}
+
+export async function getStaticPaths() {
+  // return a list of possible id values
+}
+
+export async function getStaticProps({ params }) {
+  // fetch data for the blog post, using params
 }
