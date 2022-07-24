868201d240fdee77cd6491026d01702385131e17
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-07-23 23:58:51 -0500

    Initial project page layout.

--- pages/projects.js
+++ pages/projects.js
@@ -1,11 +1,47 @@
 import Layout from "../components/Layout";
 import utilStyles from "../styles/utils.module.css";
+import projectStyles from "../styles/Project.module.css";
+import Image from "next/image";
+import { FaGithub, FaLink } from "react-icons/fa";
 
 export default function Projects() {
-  const title = "Projects";
+  const title = "Some of My Work";
+
   return (
     <Layout title={title}>
-      <h1 className={utilStyles.headingXl}>{title} Page</h1>
+      <div className={projectStyles.projectContainer}>
+        <div className={projectStyles.imgContainer}>
+          <Image
+            width={800}
+            height={300}
+            alt={""}
+            src={"/images/project-temp-img.png"}
+          />
+        </div>
+
+        <div className={projectStyles.descriptionContainer}>
+          <div>
+            <h1 className={projectStyles.heading2Xl}>Project Title</h1>
+          </div>
+          <div>
+            <p className={projectStyles.flavorText}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Asperiores, reiciendis necessitatibus cupiditate optio id
+              voluptatum consectetur ex obcaecati! Ducimus culpa, quia
+              cupiditate porro repellendus quisquam.
+            </p>
+          </div>
+        </div>
+
+        <div className={projectStyles.linkContainer}>
+          <a href="https://www.github.com">
+            <FaGithub />
+          </a>
+          <a href="https://www.github.com">
+            <FaLink />
+          </a>
+        </div>
+      </div>
     </Layout>
   );
 }
--- posts/a-simple-test.md
+++ posts/a-simple-test.md
@@ -7,7 +7,7 @@ summary: "dummy data, just testing the sort method."
 
 And now I'm checking **to see if markdown parsing** is _working_ correctly.
 
-[This is a test link]('https://www.duckduckgo.com')
+[This is a test link](https://www.duckduckgo.com)
 
 ```javascript
 const values = ["🕺🏿", false, {}, () => {}, 15];
