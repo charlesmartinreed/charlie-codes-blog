ac75f883cafd67ed847a939be32ff8b522a0ccfb
Charles Martin Reed <charlesmartinreed@icloud.com>
2022-08-13 19:11:38 -0500

    Reset search component from scratch.

--- components/Layout.js
+++ components/Layout.js
@@ -25,6 +25,7 @@ export default function Layout({ children, title, content }) {
       </Head>
 
       <main className={styles.main}>
+        <Search />
         {children}
         <Footer />
       </main>
--- components/Search.js
+++ components/Search.js
@@ -2,21 +2,13 @@ import styles from "../styles/Search.module.css";
 import { FaSearch } from "react-icons/fa";
 
 export default function Search() {
-  function handleInput(searchInput) {
-    const resultsDiv = document.querySelector(".searchResults");
-    resultsDiv.classList.add("display");
-    console.log(searchInput);
+  function handleClick() {
+    console.log("search button clicked");
   }
 
   return (
     <div className={styles.searchContainer}>
-      <input
-        placeholder="Search"
-        className={styles.searchInput}
-        type="search"
-        onChange={(e) => handleInput(e.target.value)}
-      ></input>
-      <div className={styles.searchResults}></div>
+      <FaSearch className={styles.searchIcon} onClick={() => handleClick()} />
     </div>
   );
 }
